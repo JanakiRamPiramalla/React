b9998c6503eb3f102de9da8723c3c3ff63f6d0eb
JanakiRamPiramalla <piramallajanakiram@gmail.com>
2025-06-08 16:14:58 +0530

    completed

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -1,12 +1,12 @@
-import ProfilePic from './assets/MyProfile.jpg'
+import ProfilePic from './assets/JanakiRam.jpg'
 
 function Card(){
     return(
-        <div class="card">
-            <img src="https://via.placeholder.com/150" alt="Profile Picture"></img>
+        <div ><img src={ProfilePic} style={{width: "300px", height: "400px", border: "1px solid black", borderRadius: "10px", textAlign: "center", padding: "20px"}} alt="Profile Picture"></img>
             <h2>Janaki Ram</h2>
             <p> I Watch Youtube videos and play offline Games</p>
         </div>
     );
 }
+
 export default Card
