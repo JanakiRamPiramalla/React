a99e594c0700cdfaf1e9fa09f60d510b8acbc993
JanakiRamPiramalla <piramallajanakiram@gmail.com>
2025-06-08 15:56:40 +0530

    first commit

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,11 @@
+import ProfilePic from './assets/MyProfile.jpg'
+function Card(){
+    return(
+        <div class="card">
+            <img src="https://via.placeholder.com/150" alt="Profile Picture"></img>
+            <h2>Janaki Ram</h2>
+            <p> I Watch Youtube videos and play offline Games</p>
+        </div>
+    );
+}
+export default Card
---
+++ my-react-app/src/Foot.jsx
@@ -0,0 +1,12 @@
+function Foot(){
+    const f1="Orange"
+    const f2="Grapes"
+    return(
+        <ul>
+            <li>Apple</li>
+            <li>{f1}</li>
+            <li>{f2.toUpperCase()}</li>
+        </ul>
+    );
+}
+export default Foot
